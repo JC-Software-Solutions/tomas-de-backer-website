270d048bd91e90b35d0c2fec30238a16544b3eae
Jeroen Claessens <jclaessens97@hotmail.com>
2023-01-28 23:28:04 +0100

    fix loader

--- components/FullScreenLoader.vue
+++ components/FullScreenLoader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed h-screen w-screen z-50 bg-primary flex justify-center items-center">
+  <div class="fixed h-screen w-screen z-100 bg-primary flex justify-center items-center">
     <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-secondary" />
     <NuxtImg
       src="/img/logo/TDB.svg" alt="Tomas De Backer logo" class="absolute h-16 w-16"
--- components/Hero.vue
+++ components/Hero.vue
@@ -9,29 +9,35 @@ onMounted(() => {
 })
 
 function onVideoLoaded() {
-  loading.value = false
+  setTimeout(() => {
+    loading.value = false
+  }, 500)
 }
 </script>
 
 <template>
   <FullScreenLoader v-if="loading" />
 
-  <!-- Background video -->
-  <video ref="video" autoplay muted loop cover playsinline preload="auto" @loadeddata="onVideoLoaded">
-    <source src="/video/hero.mp4" type="video/mp4">
-  </video>
+  <Transition name="video">
+    <div v-show="!loading">
+      <!-- Background video -->
+      <video ref="video" autoplay muted loop cover playsinline preload="auto" class="mt-16" @loadeddata="onVideoLoaded">
+        <source src="/video/hero.mp4" type="video/mp4">
+      </video>
 
-  <!-- Overlay -->
-  <section class="font-display text-white text-center flex flex-col align-center h-64vh sm:h-70vh" data-aos="fade-up z-0">
-    <div class="mt-40">
-      <h1 class="text-4xl md:text-7xl text-shadow-lg">
-        Tomas De Backer
-      </h1>
-      <h2 class="text-xl md:text-4xl text-shadow-lg">
-        {{ $t('hero-title') }}
-      </h2>
+      <!-- Overlay -->
+      <section class="font-display text-white text-center flex flex-col align-center h-64vh sm:h-70vh" data-aos="fade-up z-0">
+        <div class="mt-40">
+          <h1 class="text-4xl md:text-7xl text-shadow-lg">
+            Tomas De Backer
+          </h1>
+          <h2 class="text-xl md:text-4xl text-shadow-lg">
+            {{ $t('hero-title') }}
+          </h2>
+        </div>
+      </section>
     </div>
-  </section>
+  </Transition>
 </template>
 
 <style scoped>
@@ -45,5 +51,16 @@ video {
   top: 0;
   bottom: 0;
   z-index: -1;
+}
+
+/* .video-enter-active, */
+.video-enter-active {
+  opacity: 0;
+  transition: opacity 1s ease;
+}
+
+/* .video-enter-from, */
+.video-enter-to {
+  opacity: 1;
 }
 </style>
