31a4c95600c5527a4a04d670f682c0cb004a12b5
Jeroen Claessens <jclaessens97@hotmail.com>
2023-02-21 17:17:01 +0100

    update sitemap.xml

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,6 +1,19 @@
 import { SitemapStream, streamToPromise } from 'sitemap'
 import { serverQueryContent } from '#content/server'
 
+const routes = [
+  '/',
+  '/about',
+  '/calendar',
+  '/results',
+]
+
+const locales = [
+  '', // default = nl
+  '/en',
+  '/fr',
+]
+
 export default defineEventHandler(async (event) => {
   const docs = await serverQueryContent(event).find()
   const sitemap = new SitemapStream({
@@ -9,9 +22,19 @@ export default defineEventHandler(async (event) => {
   for (const doc of docs) {
     sitemap.write({
       url: doc._path,
-      changefreq: 'monthly',
+      changefreq: 'weekly',
     })
   }
+
+  routes.forEach((route) => {
+    locales.forEach((locale) => {
+      sitemap.write({
+        url: `${locale}${route}`,
+        changefreq: 'weekly',
+      })
+    })
+  })
+
   sitemap.end()
   return streamToPromise(sitemap)
 })
