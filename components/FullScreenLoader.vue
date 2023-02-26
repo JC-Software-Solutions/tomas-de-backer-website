c3fe335933890b36bd685e4846c0d9c7eca84afd
Jeroen Claessens <jclaessens97@hotmail.com>
2023-02-26 18:58:10 +0100

    renamed svg

--- components/FullScreenLoader.vue
+++ components/FullScreenLoader.vue
@@ -2,7 +2,7 @@
   <div class="fixed h-screen w-screen z-100 bg-primary flex justify-center items-center">
     <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-secondary" />
     <NuxtImg
-      src="/img/logo/TDB.svg" alt="Tomas De Backer logo" class="absolute h-16 w-16"
+      src="/img/logo/TDB-logo.svg" alt="Tomas De Backer logo" class="absolute h-16 w-16"
     />
   </div>
 </template>
