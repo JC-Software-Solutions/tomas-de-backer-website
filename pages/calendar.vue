5eea8fef2545930c05e62694629a7d2c69592969
Jeroen Claessens <jclaessens97@hotmail.com>
2023-08-01 14:09:06 +0200

    fix: missing title calendar

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -10,8 +10,8 @@ useSeoMeta({
 
 <template>
   <div class="container flex flex-col mx-auto pt-4">
-    <h3 class="text-center text-4xl">
-      My 2023 Season Calendar
+    <h3 class="text-center text-4xl capitalize">
+      {{ $t('calendar') }}
     </h3>
 
     <CalendarEntry v-for="cal in calendar?.body" :key="cal.title" :data="cal" class="my-5" />
